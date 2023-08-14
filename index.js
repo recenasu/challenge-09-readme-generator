1556786b5340153068cea2e43b9b9a5c01f8d2f9
Domitius <133902698+recenasu@users.noreply.github.com>
2023-08-14 08:20:01 -0700

    added table of contents

--- index.js
+++ index.js
@@ -60,10 +60,9 @@ inquirer
         },
     ])
     .then((response) => {
-        const err = 'Failed to add '
-
+        
         // Define variable for adding content and formatting to README file.
-        let contentsReadMe = "# Title: " + response.title + "\n\n## Description\n" + response.description + "\n\n### Installation\n\n" + response.install + "\n\n### Usage\n\n" + response.usage + "\n\n### Contributing\n\n" + response.contribution + "\n\n### Tests\n\n" + response.test + "\n\n### License\n\n" + response.license + "\n\n### Questions\n\n" + "> * GitHub Username: " + response.gitHubUserName + "\n\n" + "> * GitHub Profile: https://github.com/" + response.gitHubUserName + "\n\n> * Email: " + response.email + "\n\n> * Contact Instructions: " + response.contact + "\n\n";
+        let contentsReadMe = "# Title: " + response.title + "\n\n## Description\n" + response.description + "\n\n## Table of Contents\n\n[Installation](#installation)<br>[Usage](#usage)<br>[Contributing](#contributing)<br>[Tests](#tests)  [License](#license)<br>[Questions](#questions)\n\n### Installation\n\n" + response.install + "\n\n### Usage\n\n" + response.usage + "\n\n### Contributing\n\n" + response.contribution + "\n\n### Tests\n\n" + response.test + "\n\n### License\n\n" + response.license + "\n\n### Questions\n\n" + "> * GitHub Username: " + response.gitHubUserName + "\n\n" + "> * GitHub Profile: https://github.com/" + response.gitHubUserName + "\n\n> * Email: " + response.email + "\n\n> * Contact Instructions: " + response.contact + "\n\n";
 
         // Generate Readme file
         fs.appendFile('Generated_README.md', contentsReadMe, (err) => err ? console.error(err + "title.") : console.log('Project README created successfully!'));
